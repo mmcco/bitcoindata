b3aeeb3c94a4ae993b87f1fde5ab73e64b30d5d0
Michael McConville <mmcconville@colgate.edu>
2013-07-09 14:16:18 -0400

    adding print statements and timers

--- users.py
+++ users.py
@@ -1,8 +1,13 @@
+import time
+
+lastTime = time.time()
+
 def parseInput(inputLine):
+
     data = inputLine.split(",")
     # remember, this is only meant to parse newInputs.csv, not inputs.csv
     if len(data) != 7:
-        raise Exception("bad line in inputs - cannot parse  -==-  " + inputLine + "  -==- length of parsed input line is " + str(len(data)))
+        raise Exception("bad line in inputs - cannot parse  -==-  " + inputLine + "  -==-  length of parsed input line is " + str(len(data)))
     return data
 
 
@@ -13,10 +18,12 @@ def resizeTxs(count):
 
 # returns the given addresses's root and rank in a tuple
 def getRoot(addr):
+
     rank = 1
-    while addresses[addr] != None:
+    while addresses[addr] is not None:
         addr = addresses[addr]
         rank += 1
+
     return (addr, rank)
 
 
@@ -58,14 +65,25 @@ def union(args):
 txs = [[]]  # index is txID, value is a list of its inputs' addresses
 inputs = open("newInputs.csv", "r")
 
+print "beginning loading addresses into dictionary by transaction"
+numInputs = 0
+
 # fill a dict with a key for each address
 for line in inputs:
+
     data = parseInput(line)
     txID = int(data[0])
     if len(txs) <= txID:
         resizeTxs(txID)
     txs[txID].append(data[3])
+
+    numInputs += 1
     
+print "dictionary load took " + str(time.time() - lastTime) + " seconds"
+print str(numInputs) + " inputs were processed"
+print "creating dictionary of addresses, initializing roots"
+lastTime = time.time()
+
 addresses = dict() # associates address with user
 
 # populate addresses dict, making an index of value None for each address
@@ -73,13 +91,18 @@ for tx in txs:
     for addr in tx:
         addresses[addr] = None
 
+print "creating union dictionary took " + str(time.time() - lastTime) + " seconds"
+print "number of addresses: " + str(len(addresses))
+lastTime = time.time()
+print "beginning union-find on addresses"
+
 for tx in txs:
     if len(tx) > 1:
         union(tx)
 
-
-print "len(addresses): " + str(len(addresses))
-
+print "union-find took " + str(time.time() - lastTime) + " seconds"
+lastTime = time.time()
+print "merging all uses into a dictionary"
 
 usersDict = dict() # associates users' address sets with their roots
 
@@ -96,8 +119,16 @@ for key, value in addresses.iteritems():
     else:
         usersDict[root].append(key)
 
+print "merging users into dictionary took " + str(time.time() - lastTime) + " seconds"
+lastTime = time.time()
+print "number of users in dictionary: " + str(len(usersDict))
+print "converting users dictionary into a list"
+
 # generate a list of addresses, each index being a user, from usersDict
 users = []
 
 for key, value in usersDict.iteritems():
     users.append(value)
+
+print "converting dictionary into list took " + str(time.time() - lastTime) + " seconds"
+print "user generation completed"
