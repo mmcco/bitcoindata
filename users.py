b7442b8bd88849da5bb24e52d334e3a77118520a
Michael McConville <mmcconville@colgate.edu>
2013-07-09 12:05:20 -0400

    fixing user list generation bug (call getRoot for every address)

--- users.py
+++ users.py
@@ -1,106 +1,103 @@
-#!/usr/bin/python
-import time
-import MySQLdb
+def parseInput(inputLine):
+    data = inputLine.split(",")
+    # remember, this is only meant to parse newInputs.csv, not inputs.csv
+    if len(data) != 7:
+        raise Exception("bad line in inputs - cannot parse  -==-  " + inputLine + "  -==- length of parsed input line is " + str(len(data)))
+    return data
 
-lastTime = time.time()
 
-con = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="PS3SHA256",
-                     db="btc")
-                     #cursorclass = MySQLdb.cursors.SSCursor)
+def resizeTxs(count):
+    for i in range(0, count):
+        txs.append([])
 
-cur = con.cursor()
 
-cur.execute("select count(*) from txs;")
-# make an array with one indice for each transaction; each indice is a list of addresses combined in that transaction
-txs = [[] for i in range (0, cur.fetchone()[0])]
+# returns the given addresses's root and rank in a tuple
+def getRoot(addr):
+    rank = 1
+    while addresses[addr] != None:
+        addr = addresses[addr]
+        rank += 1
+    return (addr, rank)
 
-print "txs initialized - took " + str(time.time() - lastTime) + " seconds"
-lastTime = time.time()
 
-cur.execute("select txID, address from inputs limit 1000;")
-addresses = dict() # associates address with user
+# helper function, returns whether all items in a list are equal
+def areAllEqual(args):
+
+    for i in range (0, len(args) - 1):
+        if args[i] != args[i+1]:
+            return False
+
+    return True
+
+
+def union(args):
+
+    if len(args) < 2:
+        raise Exception("union() passed a list of length < 2")
+
+    rootInfo = map (getRoot, args)  # contains both root address and rank
+    roots = [i[0] for i in rootInfo]
+    ranks = [i[1] for i in rootInfo]
+
+    # if they already have the same root, we're done
+    if areAllEqual(roots):
+        return
+
+    # select the root with the highest rank as the parent
+    maxRank = 0
+    for root in rootInfo:
+        if root[1] > maxRank:
+            parent = root
+            maxRank = root[1]
+    
+    # make everyone point to the parent
+    for addr in roots:
+        addresses[addr] = parent[0]
+
+
+txs = [[]]  # index is txID, value is a list of its inputs' addresses
+inputs = open("newInputs.csv", "r")
 
 # fill a dict with a key for each address
-for line in cur.fetchall():
+for line in inputs:
+    data = parseInput(line)
+    txID = int(data[0])
+    if len(txs) <= txID:
+        resizeTxs(txID)
+    txs[txID].append(data[3])
+    
+addresses = dict() # associates address with user
 
-    if len(line) != 3:
-        raise Exception("bad response from database - length is " + str(len(line)) + " - first elem: " + str(line[0]) + " - second elem: " + str(line[1]))
-
-    if not line[1] in addresses:
-        # each address is declared with no parent and a rank of 1
-        addresses[line[1]] = (line[1], 1)
-
-    txs[line[0]].append(line[1])
-
-print "addresses dict populated - took " + str(time.time() - lastTime) + " seconds"
-lastTime = time.time()
+# populate addresses dict, making an index of value None for each address
+for tx in txs:
+    for addr in tx:
+        addresses[addr] = None
 
 for tx in txs:
     if len(tx) > 1:
         union(tx)
 
-print "unions completed - took " + str(time.time() - lastTime) + " seconds"
-lastTime = time.time()
 
-users = dict() # associates users' address sets with their roots
+print "len(addresses): " + str(len(addresses))
+
+
+usersDict = dict() # associates users' address sets with their roots
 
 for key, value in addresses.iteritems():
     
-    if not value in users:
-        users[value] = []
+    if value is None:
+        root = key
+    else:
+        root = getRoot(value)[0]
 
-    users[value].append(key)
-
-print "users generated - took " + str(time.time() - lastTime) + " seconds"
-lastTime = time.time()
-
-def union(*args):
-
-    if len(args) == 1:
-        raise Exception("union() passed a list of length 1")
-
-    roots = map (getRoot(), args)
-
-    # if they already have the same root, we're done
-    if areAllEqual(zip(*roots)[0]):
-        return
-
-    # select the root with the highest rank as the parent, remove it
-    maxRank = 0
-    for root in roots:
-        if root[1] > maxRank:
-            parent = root
-            maxRank = root[1]
-    roots.remove(parent)
-    
-    # make everyone point to the parent
-    for addr in zip(*roots)[0]:
-        addresses[addr][0] = parent
-
-    # increment the parent's rank
-    addresses[parent[0]][1] += max(zip(*roots)[1])
-
-
-# helper function that walks to the root and returns its key
-def getRoot(addr):
-    while addresses[addr][0] != addr:
-        addr = addresses[addr]
-    return addresses[addr]
-
-
-# helper function, returns whether all items in a list are equal
-def areAllEqual(*args):
-    if len(args) is 0:
-        return True
-
-    elif len(args) is 3:
-        return args[0] == args[1] == args[2]
+    if not root in usersDict:
+        usersDict[root] = [key]
 
     else:
-        return (args[0] == args[1]) and areAllEqual(args[2:])
+        usersDict[root].append(key)
 
+# generate a list of addresses, each index being a user, from usersDict
+users = []
 
-cur.close()
-con.close()
+for key, value in usersDict.iteritems():
+    users.append(value)
