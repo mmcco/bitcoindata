8573702ed7a6d7879f8b11b046c0a42612457a35
Michael McConville <mmcconville@colgate.edu>
2013-07-26 14:34:21 -0400

    adding more accurate/comprehensive logic to addressHistory()

--- dataStructs.py
+++ dataStructs.py
@@ -77,13 +77,6 @@ def blockRewardIDs():
     return rewardTxIDs
 
 
-#def inputAddrByTx():
-#    '''returns a two-dimensional list of the addresses associated with each tx's inputs; the index is the tx's txID'''
-#    inputTxs = []
-#    with open("bitcoinData/newInputs.csv", "r") as inputs:
-#        for line in inputs:
-            
-# this does not currently fit the spec
 def addressHistory():
     ''' Returns a dict associating every address with a list of (txID, value, spentInTx) tuples representing its history.
     spentInTx is the txID of the tx in which the output was used as an input; it is None by default.
@@ -95,25 +88,27 @@ def addressHistory():
             data = line.split(",", 6)
             if len(data) != 7:
                 raise Exception("bad line in newOutputs.csv")
-            txID, value, address = int(data[0]), int(data[3]), data[5]
-            addresses.setdefault(address, []).append([txID, value, None])
+            txID, outputIndex, value, address = int(data[0]), int(data[2]), int(data[3]), data[5]
+            addresses.setdefault(address, []).append([txID, outputIndex, value, None])
     
+    print "finished loading outputs"
+
     with open("bitcoinData/newInputs.csv", "r") as inputs:
         for line in inputs:
             data = line.split(",")
             if len(data) != 7:
                 raise Exception("bad line in newInputs.csv")
-            inputTxID, address, outputTxID = int(data[0]), data[3], int(data[5])
-            if not address in addresses:
-                raise Exception("input's address does not have any outputs")
-            # this should only work on one item
+            inputTxID, address, outputTxID, outputIndex = int(data[0]), data[3], int(data[5]), int(data[6])
+            # put input's txID with its corresponding output
             for output in addresses[address]:
-                if output[0] == outputTxID:
-                    output[2] = inputTxID
+                if output[0] == outputTxID and output[1] == outputIndex:
+                    output[3] = inputTxID
                     break
-            else:
+            else:  # triggers if outputTxID does not exist in addresses[address]
                 raise Exception("input's corresponding output could not be found in list")
             
+    print "finished loading inputs"
+
     map (sort(key=itemgetter(0)), addresses)
 
     return addresses
