82627ea117117d730f86cd3c4c60a043158f3b9d
Michael McConville <mmcconville@colgate.edu>
2013-07-28 16:51:16 -0400

    using central dataStructs module

--- users.py
+++ users.py
@@ -5,7 +5,7 @@
 # note that sets are used heavily - they're somewhat slower than lists but are more bug-resistant
 # if you're looking to make performance improvements, convert sets to lists where possible
 
-from dataStructs import getAddresses
+from dataStructs import getAddresses, inputAddresses
 
 # parses a CSV line from newInputs.csv
 def parseInput(inputLine):
@@ -57,17 +57,9 @@ def union(args):
             raise Exception("parent selected did not have the highest rank")
 
 
-txs = [set()]  # index is txID, value is a list of its inputs' addresses
-
-# fill a dict with a key for each address
-with open("bitcoinData/newInputs.csv", "r") as inputs:
-    for line in inputs:
-
-        data = parseInput(line)
-        txID, address = int(data[0]), data[3]
-        if len(txs) <= txID:
-            resizeTxs(txID)
-        txs[txID].add(address)
+# index is txID, value is a list of its inputs' addresses
+# WARNING: there is currently no duplicate check; convert to list of sets before using
+txs = inputAddresses()
     
 print "list of addresses associated by transaction filled"
 
